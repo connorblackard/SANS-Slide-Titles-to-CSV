36e9a7649caace578676232a8e1fb9acb8e7b1d0
Jeffrey Blackard <connorblackard@gmail.com>
2023-06-22 16:48:45 -0700

    Add files via upload

---
+++ SANS_Slide_Extractor.py
@@ -0,0 +1,13 @@
+import json
+
+with open('course.json') as course_json:
+  parsed_json = json.load(course_json)
+
+output = open('course_output.csv', 'w')
+
+for section in parsed_json["data"]["course"]["sections"]:
+  for subsection in section["modules"]:
+    for slide in subsection["slides"]:
+        output.write(section["name"] + "|" + subsection["name"] + "|" + str(subsection["fullNumber"]) + "." +str(slide["number"])+ "|" + slide["name"] + "|" + slide["duration"][3:8]+'\n')
+
+output.close()
