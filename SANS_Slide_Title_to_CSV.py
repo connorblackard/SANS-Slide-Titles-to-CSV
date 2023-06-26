c170ecead096ada1f42603f08799428571e7d8c9
Jeffrey Blackard <connorblackard@gmail.com>
2023-06-26 05:29:18 -0700

    Changing to H:MM:SS format

--- SANS_Slide_Title_to_CSV.py
+++ SANS_Slide_Title_to_CSV.py
@@ -8,6 +8,6 @@ output = open('course_output.csv', 'w')
 for section in parsed_json["data"]["course"]["sections"]:
   for subsection in section["modules"]:
     for slide in subsection["slides"]:
-        output.write(section["name"] + "|" + subsection["name"] + "|" + str(subsection["fullNumber"]) + "." +str(slide["number"])+ "|" + slide["name"] + "|" + slide["duration"][3:8]+'\n')
+        output.write(section["name"] + "|" + subsection["name"] + "|" + str(subsection["fullNumber"]) + "." +str(slide["number"])+ "|" + slide["name"] + "|" + slide["duration"][0:8]+'\n')
 
 output.close()
